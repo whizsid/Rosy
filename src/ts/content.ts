385a8dbaae21bfd6c2fe803d29f107b607417026
WhizSid <whizsid@aol.com>
2019-08-17 01:36:29 +0530

    Event Listeners

--- src/ts/content.ts
+++ src/ts/content.ts
@@ -1,34 +1,18 @@
 import Rosy from './rosy';
 
-const numbs = [
-	10,
-	100,
-	1000,
-	10000,
-	100000,
-	1000000,
-	10000000,
-	1,
-	11,
-	111,
-	1111,
-	11111,
-	7,
-	7768,
-	887,
-	887877,
-	88673773,
-	8867666767,
-	999999999999,
-	7847000077484,
-	70000000001,
-	5555555555555555,
-	77747100,
-	566521
-];
+const tags:string[] = ['p','a','b','i','h1','h2','h3','h4','h5','h6','span'];
 
-for(const numb of numbs){
 
-	console.log(numb);
-	console.log(Rosy(numb));
+for (const tag of tags){
+	const elmnts = document.getElementsByTagName(tag);
+
+	for(const elmnt of elmnts){
+		elmnt.addEventListener('mouseover',function(this){
+			const enabled = window.localStorage.getItem('rosy_enabled') as boolean;
+
+			if(enabled){
+
+			}
+		})
+	}
 }
